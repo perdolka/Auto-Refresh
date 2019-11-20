a811501160cee6cd38642d6652a38e0d087ceacf
perdolka <b33j3hof3r@gmail.com>
2019-11-20 21:43:44 +0200

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name     Auto-Refresh
+// @icon     https://image.flaticon.com/icons/svg/189/189687.svg
+// @include  https://sample.com* - put your site(s) here
+// ==/UserScript==
+
+(function () 
+{ 
+    var minutes = true; // false=seconds, true=minutes
+    var interval = minutes ? 60000 : 1000; 
+    var IDLE_TIMEOUT = 5; // minutes or seconds
+    var idleCounter = 0;
+
+    document.onmousemove = document.onkeypress = function () 
+    {idleCounter = 0;};
+
+    window.setInterval(function () 
+    {
+        if (++idleCounter >= IDLE_TIMEOUT) 
+        {window.location.reload();}
+    }, interval);
+}());
