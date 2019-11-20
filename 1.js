b1fafdcad10b170e5057266bdf24838a7a441166
perdolka <b33j3hof3r@gmail.com>
2019-11-20 22:07:55 +0200

    Update 1.js

--- 1.js
+++ 1.js
@@ -2,6 +2,10 @@
 // @name     Auto-Refresh
 // @icon     https://image.flaticon.com/icons/svg/189/189687.svg
 // /* Icon made by Roundicons from www.flaticon.com */
+// @homepageURL https://github.com/perdolka/Auto-Refresh
+// @supportURL  https://github.com/perdolka/Auto-Refresh/issues
+// @downloadURL https://raw.githubusercontent.com/perdolka/Auto-Refresh/master/1.js
+// @updateURL   https://raw.githubusercontent.com/perdolka/Auto-Refresh/master/1.js
 // @include  https://sample.com* - put your site(s) here
 // ==/UserScript==
 
